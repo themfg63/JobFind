86b935d20ef18c1c2bc28ad1c413c7cc7502f9cd
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-09-08 15:06:33 +0300

    companies page created

---
+++ frontend/src/LandingPage/Companies.tsx
@@ -0,0 +1,5 @@
+const Companies = () => {
+    return <div className="text-4xl"><span className="text-bright-sun-400">1000'den</span> fazla şirket tarafından güvenilir.</div>
+}
+
+export default Companies;
