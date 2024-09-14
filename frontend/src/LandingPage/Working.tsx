4806b08ff4bf1d5d3ce2d8e61e65f7b5b5cadd79
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-09-14 15:07:18 +0300

    Working page created

---
+++ frontend/src/LandingPage/Working.tsx
@@ -0,0 +1,7 @@
+const Working = () => {
+    return <div className="mt-20 pb-5">
+        <div className="text-4xl text-center font-semibold mb-3 text-mine-shaft-100">Nasıl <span className="text-bright-sun-400">Çalışır?</span></div>
+    </div>
+}
+
+export default Working;
