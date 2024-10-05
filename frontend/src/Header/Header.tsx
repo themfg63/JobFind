35dba8bbcac08005c222c8afa78e7a27f5d72dcd
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-10-05 17:53:37 +0300

    ProfilPage created

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -1,22 +1,20 @@
-import { Avatar, Indicator } from "@mantine/core";
+import {  Indicator } from "@mantine/core";
 import { IconBell, IconSettings, IconZoomReplace } from "@tabler/icons-react";
 import NavLinks from "./NavLinks";
 import { useLocation } from "react-router-dom";
+import ProfileMenu from "./ProfileMenu";
 
 const Header = () => {
     const location = useLocation();
 
     return location.pathname!="/signup" && location.pathname!="/login"?<div className="w-full bg-mine-shaft-950 px-6 text-white h-28 flex justify-between items-center font-['poppins']">
-        <div className="flex gap-2 items-center text-bright-sun-400">
+        <div className="flex gap-1 items-center text-bright-sun-400">
             <IconZoomReplace className="h-8 w-8"/>
             <div className="text-3xl font-semibold"> JobFind</div>
         </div>
         {NavLinks()}
         <div className="flex gap-3 items-center">
-            <div className="flex items-center gap-2">
-                <div>Furkan</div>
-                <Avatar src="avatar-png.webp" alt="it's me" />
-            </div>
+            <ProfileMenu />
             <div className="bg-mine-shaft-900 p-1.5 rounded-full">
                 <IconSettings stroke={1.5} />
             </div>
---
+++ frontend/src/Pages/ProfilePage.tsx
@@ -0,0 +1,10 @@
+import { Divider } from "@mantine/core";
+
+const ProfilePage = () => {
+    return <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins']">
+        <Divider mx="md" mb="xl" />
+        Profilim
+    </div>
+}
+
+export default ProfilePage;
