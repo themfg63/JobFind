abb09d3fe6571df3ebe57d890a9f8ad7cadf30ac
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-10-11 14:07:48 +0300

    backend connected

--- frontend/src/Components/SignUp/Login.tsx
+++ frontend/src/Components/SignUp/Login.tsx
@@ -1,23 +1,48 @@
 import { Button, PasswordInput, rem, TextInput } from "@mantine/core";
 import { IconAt, IconLock } from "@tabler/icons-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { loginUser } from "../../Services/UserService";
+
+const form = {
+    email: "",
+    password: ""
+}
 
 const Login = () => {
+    const [data, setData] = useState(form);
+
+    const handleChange = (event:any) => {
+        setData({...data, [event.target.name]:event.target.value});
+    }
+
+    const handleSubmit = () => {
+        loginUser(data).then((res) => {
+            console.log(res);
+        }).catch((err)=>console.log(err.response.data));
+    }
+
     return <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
         <div className="text-2xl font-semibold text-bright-sun-500">Giriş Yap</div>
         <TextInput
+            name="email"
+            value={data.email}
+            onChange={handleChange}
             withAsterisk
             leftSection={<IconAt style={{width: rem(16), height: rem(16) }} />}
             label="Email"
             placeholder="Email Adresinizi Girin"
         />
         <PasswordInput
+            name="password"
+            value={data.password}
+            onChange={handleChange}
             withAsterisk
             leftSection={<IconLock style={{width: rem(18), height: rem(18) }} stroke={1.5} />}
             label="Şifre"
             placeholder="Şifrenizi Girin"
         />
-        <Button autoContrast variant="filled">Giriş Yap</Button>
+        <Button onClick={handleSubmit} autoContrast variant="filled">Giriş Yap</Button>
         <div className="mx-auto">Hesabınız yok mu? Hemen <Link to="/signup" className="text-bright-sun-400 hover:underline">Kayıt Ol</Link></div>
     </div>
 }
--- frontend/src/Components/SignUp/SignUp.tsx
+++ frontend/src/Components/SignUp/SignUp.tsx
@@ -2,50 +2,94 @@ import { Anchor, Button, Checkbox, Group, PasswordInput, Radio, rem, TextInput }
 import { IconAt, IconLock } from "@tabler/icons-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { registerUser } from "../../Services/UserService";
 
-const SignUp = () => {
-    const [value, setValue] = useState('react');
+const form={
+    name:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+    accountType:"APPLICANT"
+}
+
+const SignUp = (props:any) => {
+    const [data, setData] = useState(form);
+
+    const handleChange = (event:any) => {
+        if(typeof(event)=="string"){
+            setData({...data, accountType:event});
+        }else{
+            setData({...data, [event.target.name]:event.target.value});
+        }
+    }
+
+    const handleSubmit = () => {
+        registerUser(data).then((res) => {
+            console.log(res);
+        }).catch((err) => console.log(err));
+    }
 
    return <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
     <div className="text-2xl font-semibold text-bright-sun-500">Hesap Oluştur</div>
     <TextInput
+        name="name"
+        value={data.name}
+        onChange={handleChange}
         withAsterisk
         label="Ad Soyad"
         placeholder="Adınızı ve Soyadınızı Girin"
     />
     <TextInput
+        name="email"
+        value={data.email}
+        onChange={handleChange}
         withAsterisk
         leftSection={<IconAt style={{width: rem(16), height: rem(16)}}  />}
         label="Email"
         placeholder="Email Adresinizi Girin"
     />    
     <PasswordInput
+        name="password"
+        value={data.password}
+        onChange={handleChange}
         withAsterisk
         leftSection={<IconLock style={{width: rem(18), height: rem(18)}} stroke={1.5}/>}
         label="Şifre"
         placeholder="Şifrenizi Girin"
     />
     <PasswordInput
+        name="confirmPassword"
+        value={data.confirmPassword}
+        onChange={handleChange}
         withAsterisk
         leftSection={<IconLock style={{width: rem(18), height: rem(18)}} stroke={1.5}/>}
         label="Şifre Tekrar"
         placeholder="Şifrenizi Tekrar Girin"
     />
     <Radio.Group
-        value={value}
-        onChange={setValue}
-        name="hesapTipi"
+        value={data.accountType}
+        onChange={handleChange}
         label="Hesap Tipini Seçiniz"
         withAsterisk
     >
         <Group mt="xs">
-            <Radio value="user" label="Kullanıcı" />
-            <Radio value="company" label="Firma" />
-            <Radio value="admin" label="Yönetici" />
+            <Radio 
+                className="py-4 px-6 border hover:bg-mine-shaft-900 has-[:checked]:bg-bright-sun-400/5 has-[:checked]:border-bright-sun-400 border-mine-shaft-800 rounded-lg"
+                autoContrast
+                value="APPLICANT" label="İş Arayan"
+            />
+            <Radio
+                className="py-4 px-6 border hover:bg-mine-shaft-900 has-[:checked]:bg-bright-sun-400/5 has-[:checked]:border-bright-sun-400 border-mine-shaft-800 rounded-lg"
+                autoContrast
+                value="EMPLOYER" label="İş Veren"
+            />
         </Group>
     </Radio.Group>
-    <Checkbox autoContrast label={<><Anchor>Şartlar ve Koşulları</Anchor> Kabul Ediyorum {' '}</>} />
-    <Button autoContrast variant="filled">Kayıt Ol</Button>
+    <Checkbox 
+        autoContrast 
+        label={<><Anchor>Şartlar ve Koşulları</Anchor> Kabul Ediyorum {' '}</>}
+    />
+    <Button onClick={handleSubmit} autoContrast variant="filled">Kayıt Ol</Button>
     <div className="mx-auto">Zaten bir hesabınız var mı? <Link to="/login" className="text-bright-sun-400 hover:underline">Giriş Yap</Link></div>
    </div>
 }
