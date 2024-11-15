3a268be5ac1ed56bbe1c4f8d6d728ed0bbbd4571
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-11-16 00:31:31 +0300

    ApplyJobPage updated

--- frontend/src/Pages/ApplyJobPage.tsx
+++ frontend/src/Pages/ApplyJobPage.tsx
@@ -1,28 +1,17 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom"
-import { getJob } from "../Services/JobService";
 import { Button, Divider } from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
 import ApplyJobComp from "../Components/ApplyJob/ApplyJobComp";
 
 const ApplyJobPage = () => {
     const navigate = useNavigate();
-    const {id} = useParams();
-    const [job,setJob] = useState<any>(null);
-
-    useEffect(() => {
-        window.scrollTo(0,0);
-        getJob(id).then((res) => {
-            setJob(res);
-        }).catch((err) => {
-            console.log(err);
-        })
-    },[id])
 
     return <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins'] p-4">
-        <Divider size="xs" />
-        <Button my="md" color="brightSun.4" onClick={() => navigate(-1)} leftSection={<IconArrowLeft size={20}/>} variant="light">Geri Dön</Button>
-        <ApplyJobComp {...job} />
+        <Divider size="xs" mb="xs" />
+        <Button color="brightSun.4" onClick={() => navigate(-1)} leftSection={<IconArrowLeft size={20} />} variant="light">
+            Geri Dön
+        </Button>
+        <ApplyJobComp />
     </div>
 }
 
